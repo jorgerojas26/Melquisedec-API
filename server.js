e792152a1ceea2fbccb91f4d98b0491a261c703e
Jorge Rojas <jorgeluisrojasb@gmail.com>
2021-06-15 02:37:04 -0400

    Products will now be correctly registered and updated with images

--- server.js
+++ server.js
@@ -1,45 +1,46 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 
-const client_routes = require('./routes/client');
-const user_routes = require('./routes/user');
-const supplier_routes = require('./routes/supplier');
-const category_routes = require('./routes/category');
-const product_routes = require('./routes/product');
-const product_variant_routes = require('./routes/product_variant');
+const client_routes = require("./routes/client");
+const user_routes = require("./routes/user");
+const supplier_routes = require("./routes/supplier");
+const category_routes = require("./routes/category");
+const product_routes = require("./routes/product");
+const product_variant_routes = require("./routes/product_variant");
 
-const errorMiddleware = require('./middlewares/errorMiddleware');
+const errorMiddleware = require("./middlewares/errorMiddleware");
 
 var storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-        console.log('hola from multer');
-        console.log(__dirname);
-        callback(null, path.join(__dirname, 'client', 'build', 'productImages'));
-    },
-    filename: function (req, file, callback) {
-        console.log('hola from multer filename');
-        callback(null, file.originalname);
-    },
+  destination: function (req, file, callback) {
+    console.log("hola from multer");
+    console.log(__dirname);
+    callback(null, path.join(__dirname, "client", "build", "productImages"));
+  },
+  filename: function (req, file, callback) {
+    console.log("hola from multer filename");
+    callback(null, file.originalname);
+  },
 });
 var upload = multer({ storage });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join("client", "build")));
 
 //routes
-app.use('/api/clients', client_routes);
-app.use('/api/users', user_routes);
-app.use('/api/suppliers', supplier_routes);
-app.use('/api/categories', category_routes);
-app.use('/api/products', upload.array('images', 100), product_routes);
-app.use('/api/product_variants', product_variant_routes);
+app.use("/api/clients", client_routes);
+app.use("/api/users", user_routes);
+app.use("/api/suppliers", supplier_routes);
+app.use("/api/categories", category_routes);
+app.use("/api/products", upload.array("images", 100), product_routes);
+app.use("/api/product_variants", product_variant_routes);
 
 app.use(errorMiddleware);
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log('Server running in port: ' + process.env.PORT || 5000);
+  console.log("Server running in port: " + process.env.PORT || 5000);
 });
